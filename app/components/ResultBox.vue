4464b1e569b3c96ad027bea65dbee7d02e11fd77
Javad Mnjd <javad.mnjd@hotmail.com>
2024-08-13 02:03:53 +0330

    feat: reimplement using vue computed

--- app/components/ResultBox.vue
+++ app/components/ResultBox.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-[120px] rounded-lg bg-[#373b41] p-3 text-sm">
+  <div
+    class="flex min-h-[120px] min-w-0 shrink-0 flex-col flex-wrap gap-0.5 rounded-lg bg-[#373b41] p-3 text-sm">
     <slot></slot>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,178 +2,120 @@
   <div class="flex h-screen items-center justify-center">
     <div
       class="flex w-full max-w-[480px] flex-col gap-4 rounded-xl bg-[#282a2e] p-6 drop-shadow-xl">
-      <label for="numerals" class="flex flex-col">
+      <label class="flex flex-col">
         Input alternative numeric values:
-        <input id="numerals" type="text" placeholder="0123456789" />
+        <input v-model="numerals" type="text" placeholder="0123456789" />
       </label>
-      <label for="in-decimal" class="flex flex-col">
+      <label class="flex flex-col">
         numeric value of input characters:
         <input
-          id="in-decimal"
+          :value="numericValues"
           type="text"
           placeholder="0,1,2,3,4,5,6,7,8,9"
-          disabled />
+          readonly />
       </label>
-      <input id="applyBtn" type="button" value="Apply" />
-      <div id="calculator"></div>
-      <div id="resultContainer" class="mb-5 flex justify-between gap-3">
-        <ResultBox id="resultBox" class="flex-1"></ResultBox>
-        <ResultBox id="resultBox2" class="flex-1"></ResultBox>
+
+      <div class="mb-5 flex w-full justify-between gap-3">
+        <ResultBox class="flex-1">
+          <div>Custom Numerals:</div>
+          <div>
+            {{ numerals.split('').join(', ') }}
+          </div>
+          <div>Base: {{ base }}</div>
+        </ResultBox>
+        <ResultBox class="flex-1">
+          <div>In decimal:</div>
+          <div>
+            {{ numericValues }}
+          </div>
+          <div>Base: {{ base }}</div>
+        </ResultBox>
       </div>
-      <label for="calculation" class="flex flex-col">
+      <label class="flex flex-col">
         Contraculation box:
-        <input id="calculation" type="text" placeholder="user input" />
+        <input v-model="expression" type="text" placeholder="user input" />
       </label>
+      <div>
+        <div>Result:</div>
+        <ResultBox class="flex-1">
+          <template v-if="expressionResult">
+            <div>{{ expression }} = {{ expressionResultInCustomBase }}</div>
+            <div>
+              {{ expressionToEvaluate }} =
+              {{ expressionResult }}
+            </div>
+          </template>
+        </ResultBox>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-onMounted(() => {
-  let base = 10;
-  let customNumerals = '0123456789';
-  const inDecimal = document.getElementById('in-decimal');
-
-  // Activate the placeholder for the input fields
-  function activatePlaceholder() {
-    const numeralsField = document.getElementById('numerals');
-    numeralsField.placeholder = '0123456789';
-    updateInDecimalPlaceholder(customNumerals.length);
+const numerals = ref('');
+const base = computed(() => numerals.value.length);
+const numericValues = computed(() => {
+  let values = '';
+  for (let i = 0; i < numerals.value.length; i++) {
+    values += i + ', ';
   }
-
-  // Function to update the result box with base and custom numerals
-  function updateResultBox() {
-    const howbase = charSeparator();
-    const resultBox = document.getElementById('resultBox');
-    resultBox.innerHTML = 'Custom Numerals: ' + howbase + '<br>Base: ' + base;
-  }
-
-  // Function to update the result box 2 with decimal and default numerals
-  function updateResultBox2() {
-    const howbase = charSeparator();
-    const resultBox2 = document.getElementById('resultBox2');
-    resultBox2.innerHTML =
-      'in decimal: ' + inDecimal.placeholder + '<br>Base: ' + base;
-  }
-
-  // Function to put "," between each character
-  function charSeparator() {
-    let newbase = '';
-    for (let i = 0; i < customNumerals.length; i++) {
-      newbase += customNumerals[i];
-      if (i < customNumerals.length - 1) {
-        newbase += ',';
-      }
-    }
-    return newbase;
-  }
-
-  function charGuide(customNumerals) {
-    let guide = '';
-    for (let i = 0; i < customNumerals.length; i++) {
-      guide += i + ' = ' + customNumerals[i] + '\n';
-    }
-    return guide;
-  }
-
-  // Function to update the placeholder of inDecimal
-
-  function updateInDecimalPlaceholder(length) {
-    let placeholderText = '';
-    for (let i = 0; i < length; i++) {
-      placeholderText += i;
-      if (i < length - 1) {
-        placeholderText += ',';
-      }
-    }
-    inDecimal.placeholder = placeholderText || '0';
-  }
-
-  // Event listener to update the inDecimal placeholder based on the numerals input length
-  document.getElementById('numerals').addEventListener('input', function () {
-    updateInDecimalPlaceholder(this.value.length);
-  });
-
-  // Apply button functionality
-  document.getElementById('applyBtn').addEventListener('click', function () {
-    customNumerals = document.getElementById('numerals').value;
-    // inDecimal.value = document.getElementById("numerals").value;
-    base = customNumerals.length; // Update the base
-    updateInDecimalPlaceholder(customNumerals.length); // Update inDecimal placeholder
-    updateResultBox();
-    updateResultBox2();
-  });
-
-  // Calculation input functionality
-  document
-    .getElementById('calculation')
-    .addEventListener('keypress', function (e) {
-      if (e.key === 'Enter') {
-        const calculation = document.getElementById('calculation').value;
-        // Perform calculation using custom numerals
-        const result = evaluateCalculation(calculation);
-        document.getElementById('resultBox').innerHTML +=
-          '<br>  ' + calculation + ' = ' + result;
-        document.getElementById('resultBox').style.display = 'block';
-
-        document.getElementById('resultBox2').innerHTML +=
-          '<br>  ' + calculation + ' = ' + pResult;
-        document.getElementById('resultBox2').style.display = 'block';
-        // Clear the calculation input field
-        document.getElementById('calculation').value = '';
-      }
-      console.log('R1');
-    });
-
-  // Function to evaluate the calculation
-  function evaluateCalculation(calculation) {
-    // Replace custom numerals with standard numerals
-    for (let i = 0; i < customNumerals.length; i++) {
-      calculation = calculation.split(customNumerals[i]).join(i.toString());
-    }
-    // Evaluate the modified calculation
-    return evaluate(calculation);
-  }
-
-  // Function to evaluate the modified calculation
-  function evaluate(calculation) {
-    // Split the calculation into parts
-    const parts = calculation.split(/([\+\-\*\/])/);
-    // Initialize result with the first number
-    let result = parseInt(parts[0], base);
-    // Iterate over the parts
-    for (let i = 1; i < parts.length; i += 2) {
-      const operator = parts[i];
-      const operand = parseInt(parts[i + 1], base);
-      // Perform the operation
-      if (operator === '+') {
-        result += operand;
-      } else if (operator === '-') {
-        result -= operand;
-      } else if (operator === '*') {
-        result *= operand;
-      } else if (operator === '/') {
-        result /= operand;
-      }
-    }
-
-    // Convert the result to a string representation in the custom base
-    return convertToCustomBase(result);
-  }
-
-  // Function to convert a number to a string representation in the custom base
-  function convertToCustomBase(number) {
-    let customNumber = '';
-    while (number > 0) {
-      const remainder = number % base;
-      customNumber = customNumerals[remainder] + customNumber;
-      number = Math.floor(number / base);
-    }
-    return customNumber || customNumerals[0];
-  }
-
-  // Initial update of the result box
-  updateResultBox();
-  updateResultBox2();
+  values = values.replace(/,\s*$/, '');
+  return values;
 });
+
+const expression = ref('');
+
+const expressionToEvaluate = computed(() => {
+  // Replace custom numerals with standard numerals
+  let val = expression.value;
+  for (let i = 0; i < numerals.value.length; i++) {
+    val = val.split(numerals.value.charAt(i)).join(i.toString());
+  }
+  return val;
+});
+
+const expressionResult = computed(() => {
+  return evaluate(expressionToEvaluate.value);
+});
+
+const expressionResultInCustomBase = computed(() => {
+  return convertToCustomBase(expressionResult.value);
+});
+
+// Function to evaluate the modified calculation
+function evaluate(expressionToEvaluate: string) {
+  // Split the calculation into parts
+  const parts = expressionToEvaluate.split(/([+\-*/])/);
+  // Initialize result with the first number
+  let result = parseInt(parts[0] || '0', base.value);
+  // Iterate over the parts
+  for (let i = 1; i < parts.length; i += 2) {
+    const operator = parts[i];
+    const operand = parseInt(parts[i + 1] || '0', base.value);
+    // Perform the operation
+    if (operator === '+') {
+      result += operand;
+    } else if (operator === '-') {
+      result -= operand;
+    } else if (operator === '*') {
+      result *= operand;
+    } else if (operator === '/') {
+      result /= operand;
+    }
+  }
+
+  // Convert the result to a string representation in the custom base
+  return result;
+}
+
+// Function to convert a number to a string representation in the custom base
+function convertToCustomBase(number: number) {
+  let customNumber = '';
+  while (number > 0) {
+    const remainder = number % base.value;
+    customNumber = numerals.value[remainder] + customNumber;
+    number = Math.floor(number / base.value);
+  }
+  return customNumber || numerals.value[0];
+}
 </script>
